6cf393be25a65f9a8dea46981a96c5522dbfad73
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-15 06:21:37 +0500

    changes done

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,7 +3,7 @@ const CTA = ({ text, icon, bg, color, border, hover, href, iconCss }) => {
     return (
         <>
             <div dangerouslySetInnerHTML={{
-                __html: `<a href=${href} class="h-[50px] px-3 2xl:px-6 rounded-md text-[16px] xl:text-[18px] font-normal font-sans flex items-center gap-x-2 ${color} ${bg} ${hover} ${border}"> 
+                __html: `<a href=${href} class="h-[40px] xl:h-[50px] px-3 2xl:px-6 rounded-md text-[16px] xl:text-[18px] font-normal font-sans flex items-center gap-x-2 ${color} ${bg} ${hover} ${border}"> 
                 <span class="text-[16px] xl:text-[18px] font-normal">${text}</span>
                 <img src=${icon} alt="Icons" class="object-contain ${iconCss}" />
                 </a>`
