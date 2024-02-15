b52f8e0b201aeda7e3d927179dc7e5618dea1ccd
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-16 04:48:21 +0500

    loader fixed

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -9,7 +9,7 @@ const Loader = () => {
     return (
         <>
             <section className={`${styles.loader} fixed left-0 top-0 w-[100%] h-[100vh]  z-50 flex flex-col items-center justify-center bg-whit`}>
-                <Image src={logo} priority className="w-[12%]" alt="Infinity Animation" />
+                <Image src={logo} priority className="w-[50%] lg:w-[12%]" alt="Infinity Animation" />
                 <p className="pt-5"></p>
             </section>
         </>
