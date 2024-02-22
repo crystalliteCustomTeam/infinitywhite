6a627280f070666693850829ef8eb957cc9e2c91
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-22 05:56:23 +0500

    all fixes done

--- src/app/thank-you/layout.js
+++ src/app/thank-you/layout.js
@@ -4,12 +4,12 @@ export const metadata = {
     title: "Thank You - Infinity Animation",
     description: "Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame. ",
     // Canonical
-    alternates: { canonical: 'https://infinityanimationspro.com/why-us/' },
+    alternates: { canonical: 'https://infinityanimationspro.com/thank-you/' },
     // OG Metas
     openGraph: {
         title: 'Thank You - Infinity Animation',
         description: 'Learn more about the passionate team driving innovation, precision, and storytelling mastery in every animated frame. ',
-        url: 'https://infinityanimationspro.com/why-us/',
+        url: 'https://infinityanimationspro.com/thank-you/',
      siteName: 'Infinity Animation',
         locale: 'en_US',
         type: 'website',
