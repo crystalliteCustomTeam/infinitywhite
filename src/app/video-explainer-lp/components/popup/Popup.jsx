277ca76d3a51809c4e211d8101a25a4b49ff3cb9
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-24 01:08:56 +0500

    popup fixes

--- src/app/video-explainer-lp/components/popup/Popup.jsx
+++ src/app/video-explainer-lp/components/popup/Popup.jsx
@@ -137,18 +137,18 @@ const Popup = ({ }) => {
         <>
             <Dialog open={popup} handler={popupHandle} className='popup h-full w-full' style={{ background: 'rgba(0, 0, 0, 0.4)', backdropFilter: 'blur(8px)' }}>
                 <section className='h-full lg:w-full py-12 px-6'>
-                    <div className='popupBg lg:bg-[url("../../public/video-explainer/popup-bg.png")] bg-[length:100%_100%] bg-center bg-no-repeat max-w-4xl mx-auto relative top-[50px] md:top-[200px] lg:top-0 bottom-0 left-0 right-0 lg:h-full w-full'>
-                        <div className='w-[50px] h-[50px] absolute top-[2%] right-[2%] flex items-center justify-center text-center rounded-[50%] border-0 cursor-pointer'>
+                    <div className='popupBg bg-none lg:bg-[url("../../public/video-explainer/popup-bg.png")] bg-[length:100%_100%] bg-center bg-no-repeat max-w-4xl lg:max-w-3xl xl:max-w-4xl 3xl:max-w-5xl mx-auto relative top-[0px] md:top-[100px] lg:top-[-5%] xl:lg:top-0 3xl:top-[75px] bottom-0 left-0 right-0 h-auto lg:h-[110%] xl:lg:h-full 3xl:h-[85%] w-full pb-10 md:pb-16'>
+                        <div className='w-[40px] xl:w-[50px] h-[40px] xl:h-[50px] absolute top-[2%] right-[2%] flex items-center justify-center text-center rounded-[50%] border-0 cursor-pointer'>
                             <Image onClick={popupHandle} src={btnClose} className="w-full" alt='Infinity Animations' />
                         </div>
-                        <div className="grid grid-cols-12 absolute right-[5%] top-[12%]">
+                        <div className="grid grid-cols-12 lg:w-5/12 lg:absolute right-[5%] top-[12%] xxl:top-[15%] pt-16 md:pt-20 lg:pt-0">
                             <div className="col-span-12">
-                                <h3 className='text-[20px] text-black font-bold text-center p-0 montserrat mb-0'>Fill This Form to Avail</h3>
-                                <h2 className='text-[36px] text-[#EF0707] text-center font-bold montserrat mb-0'>Amazing Discounts</h2>
-                                <h4 className='text-[20px] text-black text-center font-bold montserrat'>on Video Animation Services</h4>
+                                <h3 className='text-[16px] xl:text-[20px] text-black font-bold text-center p-0 montserrat mb-0'>Fill This Form to Avail</h3>
+                                <h2 className='text-[24px] xl:text-[36px] text-[#EF0707] text-center font-bold montserrat mb-0'>Amazing Discounts</h2>
+                                <h4 className='text-[16px] xl:text-[20px] text-black text-center font-bold montserrat'>on Video Animation Services</h4>
                                 <form action="javascript:;" className='mx-auto px-5 mt-5 h-full'>
                                     <div className="name mt-1 relative w-full">
-                                        <input type="text" name="name" placeholder='Enter Your Name' className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
+                                        <input type="text" name="name" placeholder='Enter Your Name' className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] lg:h-[40px] xl:h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
                                         {errors.name && (
                                             <span className="absolute left-[5px] bottom-[-5px] text-[11px] block font-medium text-[#EF0707]">
                                                 {errors.name}
@@ -156,7 +156,7 @@ const Popup = ({ }) => {
                                         )}
                                     </div>
                                     <div className="email mt-1 relative w-full">
-                                        <input type="email" name="email" placeholder='Enter Email Address' className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
+                                        <input type="email" name="email" placeholder='Enter Email Address' className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] lg:h-[40px] xl:h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
                                         {errors.email && (
                                             <span className="absolute left-[5px] bottom-[-5px] text-[11px] block font-medium text-[#EF0707]">
                                                 {errors.email}
@@ -164,7 +164,7 @@ const Popup = ({ }) => {
                                         )}
                                     </div>
                                     <div className="phone mt-1 relative w-full">
-                                        <input type="phone" name="phone" placeholder='Enter Phone Number' minLength={7} maxLength={15} className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
+                                        <input type="phone" name="phone" placeholder='Enter Phone Number' minLength={7} maxLength={15} className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] h-[50px] lg:h-[40px] xl:h-[50px] w-full mb-[10px] font-[500] rounded-[6px] shadow-md' onChange={handleDataChange} required />
                                         {errors.phone && (
                                             <span className="absolute left-[5px] bottom-[-5px] text-[11px] block font-medium text-[#EF0707]">
                                                 {errors.phone}
@@ -175,14 +175,14 @@ const Popup = ({ }) => {
                                         <textarea name='message' placeholder='Message' className='text-[14px] text-black placeholder:text-[#D5D5D5] focus:outline-none montserrat px-[18px] py-[12px] h-[100px] w-full font-[500] rounded-[6px] shadow-md resize-none' onChange={handleDataChange}></textarea>
                                     </div>
                                     <div className="btn mt-2 w-full text-end">
-                                        <button type='submit' className='h-[45px] w-max px-[25px] border-0 bg-black text-white text-[15px] font-[600] poppins rounded-[6px] ml-auto' onClick={handleFormSubmit} disabled={isDisabled}>
+                                        <button type='submit' className='h-[45px] w-max px-[50px] lg:px-[25px] border-0 bg-black text-white text-[15px] font-[600] poppins rounded-[6px] ml-auto' onClick={handleFormSubmit} disabled={isDisabled}>
                                             {formStatus}
                                         </button>
                                     </div>
                                 </form>
                             </div>
                         </div>
-                        <div className='w-[20%] h-[40px] absolute top-[0%] left-[65%] flex items-center justify-center text-center border-0 bg-[#EF0707] text-white text-[14px] font-bold montserrat uppercase rounded-t-none rounded-[10px]'>
+                        <div className='w-[50%] md:w-[30%] lg:w-[20%] h-[40px] absolute top-[0%] left-[25%] md:left-[35%] lg:left-[65%] flex items-center justify-center text-center border-0 bg-[#EF0707] text-white text-[14px] font-bold montserrat uppercase rounded-t-none rounded-[10px]'>
                             <span>SPECIAL OFFER</span>
                         </div>
                     </div>
