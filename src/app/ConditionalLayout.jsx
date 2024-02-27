f4f6cedbab8555bc92a5eb8d70f0714d77bf01f7
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-28 03:46:06 +0500

    Add Country Restrictions

--- src/app/ConditionalLayout.jsx
+++ src/app/ConditionalLayout.jsx
@@ -6,8 +6,24 @@ import { PopupProvider } from "./configs/store/Popup"
 import Footer from "@/components/footer/Footer"
 import Header from "@/components/header/Header"
 import Loader from "@/components/loader/Loader"
+import Axios from "axios"
 
 const ConditionalLayout = ({ children }) => {
+    const [country, setCountry] = useState(false);
+    const getIPData = async () => {
+        const res = await Axios.get(
+            "https://api.ip2location.io/?key=F9B01293761EF666EB54678698AC8682"
+        );
+        setCountry(res.country_code)
+    };
+    useEffect(() => {
+        getIPData()
+        // Redirect logic based on country
+        if (country === 'IN' || country === 'PK' || country === 'BD') {
+          // Redirect to a specific page for these countries
+          window.location.href = 'https://jumpto1.com';
+        }
+      }, [country]);
     // Loader
     const [imagesLoaded, setImagesLoaded] = useState(false);
     useEffect(() => {
