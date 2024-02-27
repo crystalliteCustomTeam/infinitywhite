8560dc7cb5587925d3e53f977a46568e63286f80
cl-farooqaziz <dev.faz@productions-dev.com>
2024-02-28 02:33:38 +0500

    geo location fixes

--- src/app/middleware/Geolocation.js
+++ src/app/middleware/Geolocation.js
@@ -1,17 +1,17 @@
 import geoip from 'geoip-lite';
 
-const BLOCKED_COUNTRY = 'PK';
+const BLOCKED_COUNTRIES = ['IN', 'PK', 'BD', 'IR', 'VN', 'PH', 'IQ', 'AF', 'KP', 'NP', 'LK', 'RU', 'CN', 'BT'];
 
 export default function geoLocationMiddleware(req, res, next) {
     let ip = req.headers['x-forwarded-for'] || req.headers['x-real-ip'] || req.connection.remoteAddress;
-    
-    if(ip && ip.includes(',')) {
+
+    if (ip && ip.includes(',')) {
         ip = ip.split(',')[0].trim();
     }
 
     const geo = geoip.lookup(ip);
 
-    if (geo && geo.country === BLOCKED_COUNTRY) {
+    if (geo && BLOCKED_COUNTRIES.includes(geo.country)) {
         return res.status(403).send('Access Denied');
     }
     next();
